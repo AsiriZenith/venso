27b0c53518f27e4a501f26866c591e74eed2a6d6
asirimanjitha <asirisenith@gmail.com>
2023-09-16 11:45:56 +0530

    created components and added code respective code to them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { AboutComponent } from './components/about/about.component';
+import { RoomComponent } from './components/room/room.component';
+import { VideoComponent } from './components/video/video.component';
+import { ServiceComponent } from './components/service/service.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { TeamComponent } from './components/team/team.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    CarouselComponent,
+    BookingComponent,
+    AboutComponent,
+    RoomComponent,
+    VideoComponent,
+    ServiceComponent,
+    TestimonialComponent,
+    TeamComponent,
+    NewsletterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
